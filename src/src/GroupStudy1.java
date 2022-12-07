e0f1c04bb6d54b6fef04c7713c7f652150fc6c24
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2022-12-08 00:10:45 +0300

    GroupStudy1 project is created.

---
+++ src/src/GroupStudy1.java
@@ -0,0 +1,16 @@
+public class GroupStudy1 {
+    /*Reverse the given String by using as many as different methods*/
+    public static void main(String[] args) {
+
+        System.out.println("Hello Group-7. Get Better Together!");
+        reverseMethod1("Cydeo School");
+        reverseMethod2("Software Development in Test-SDET");
+    }
+    public static void reverseMethod1(String str){
+
+    }
+    public static void reverseMethod2(String str){
+
+    }
+
+}
