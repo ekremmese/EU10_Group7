15c428c67ce5b7b3402ab849b991dab792a3a488
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2022-12-08 00:30:28 +0300

    Hi Guys! Let's do it...

--- src/src/GroupStudy1.java
+++ src/src/GroupStudy1.java
@@ -1,10 +1,10 @@
 public class GroupStudy1 {
-    /*Reverse the given String by using as many as different methods*/
+    /*Please reverse the given String by using as many as different methods*/
     public static void main(String[] args) {
 
         System.out.println("Hello Group-7. Get Better Together!");
-        reverseMethod1("Cydeo School");
-        reverseMethod2("Software Development in Test-SDET");
+        reverseMethod1("CYDEO School");
+        reverseMethod2("Software Development Engineer in Test-SDET");
     }
     public static void reverseMethod1(String str){
 
