ad340cdedb8eb64ca572d9cf47e0f57f63335697
Bunyamin Saglam <saglambunyamin@gmail.com>
2022-12-08 21:46:23 +0300

    Benjamin_Math created...

---
+++ src/Benjamin_Math.java
@@ -0,0 +1,12 @@
+public class Benjamin_Math {
+
+    public static void main(String[] args) {
+        System.out.println(remainingBy11(98765));
+    }
+
+    public static int remainingBy11 (int number) {
+        int result = number%11;
+        return result;
+    }
+
+}
