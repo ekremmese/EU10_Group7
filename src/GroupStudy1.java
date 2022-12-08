a5b07b6f3f5bfc6ddef19f577b235abdb96a2b9a
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2022-12-08 10:44:43 +0300

    StringBuffer() method is used for reversing

--- src/GroupStudy1.java
+++ src/GroupStudy1.java
@@ -7,14 +7,24 @@ public class GroupStudy1 {
         reverseMethod1("CYDEO School");
 
         reverseMethod2("Software Development Engineer in Test-SDET");
+
+        reverseMethod3("Hello Group-7. Get Better Together!");
     }
-    public static void reverseMethod1(String str){
+
+    public static void reverseMethod1(String str) {
 
     }
-    public static void reverseMethod2(String str){
+
+    public static void reverseMethod2(String str) {
 
     }
-    public static void reverseMethod3(String str){
+
+    public static void reverseMethod3(String str) {
+        /*StringBuffer is a peer class of String that provides much of the functionality of strings.
+        * StringBuffer class is used to create mutable (modifiable) string.
+        * The StringBuffer class in java is same as String class except it is mutable i.e. it can be changed. */
+        StringBuffer temp = new StringBuffer(str);
+        System.out.println(temp.reverse());
 
     }
 
