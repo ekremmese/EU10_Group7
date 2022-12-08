f200244599283c92c59dbf37031bb4824cf31a54
Ekrem Meşe <ekremmese1@gmail.com>
2022-12-08 23:18:31 +0300

    New codes added!

--- src/Ekrem2.java
+++ src/Ekrem2.java
@@ -2,5 +2,7 @@ public class Ekrem2 {
     public static void main(String[] args) {
 
         System.out.println( "This is for pull request");
+
+        "I want Bümnyamin to remove this message"
     }
 }
